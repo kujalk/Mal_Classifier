4b9033655ade25939484038003069a77035d3c75
Kugathasan Janarthanan <34956146+kujalk@users.noreply.github.com>
2019-06-20 18:12:39 +0800

    Add files via upload

---
+++ node_js/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
+var db;
+
+//Establish Connection
+MongoClient.connect('mongodb://localhost:27017/results', function (err, database) {
+   if (err) 
+   	throw err
+   else
+   {
+	db = database;
+	console.log('Connected to MongoDB');
+	//Start app only after connection is ready
+	app.listen(3000,'193.168.3.194');
+        console.log('I am now listening....')
+   }
+ });
+
+app.use(bodyParser.json())
+
+app.post('/', function(req, res) {
+   // Insert JSON straight into MongoDB
+  db.collection('post_data').insert(req.body, function (err, result) {
+      if (err)
+         res.send('Error');
+      else
+        res.send('Success');
+
+  });
+});
